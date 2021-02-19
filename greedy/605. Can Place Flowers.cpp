a7bac1090208c0f792c70b36d36db89e569610d3
snowlotus <541364125@qq.com>
2021-02-19 17:05:56 +0800

    Create 605. Can Place Flowers.cpp

---
+++ greedy/605. Can Place Flowers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        flowerbed.insert(flowerbed.begin(), 0);
+        flowerbed.push_back(0);
+        int res = 0, len = flowerbed.size();
+        for (int i = 1; i < len - 1; i++) {
+            if (flowerbed[i] == 0 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1;
+                res++;
+            }
+        }
+        return res >= n;
+        
+    }
+};
