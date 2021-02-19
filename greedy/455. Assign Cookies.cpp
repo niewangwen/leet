677983724b959b608d4c98f9224dd265b6414fea
snowlotus <541364125@qq.com>
2021-02-19 12:08:27 +0800

    Create 455. Assign Cookies.cpp

---
+++ greedy/455. Assign Cookies.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        //first of all, sort 2 vecs
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int res = 0, i = 0, j = 0;
+        //strategy: for each child with g[i], assign the cookie(s[i]) which is JUST larger than g[i] to him/her
+        while (i < g.size() && j < s.size()) {
+            if (s[j] >= g[i]) {
+                res++;
+                i++;
+            }
+            j++;
+        }
+        return res;
+    }
+};
