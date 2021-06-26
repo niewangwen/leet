ebb3204d1b5769c43a5f0829bfebe37646128f9f
snowlotus <541364125@qq.com>
2021-06-26 22:13:37 +0800

    Create 390. Elimination Game.cpp

---
+++ math/390. Elimination Game.cpp
@@ -0,0 +1,7 @@
+// https://www.cnblogs.com/grandyang/p/5860706.html
+class Solution {
+public:
+    int lastRemaining(int n) {
+        return n == 1 ? 1 : 2 * (1 + n / 2 - lastRemaining(n / 2));
+    }
+};
