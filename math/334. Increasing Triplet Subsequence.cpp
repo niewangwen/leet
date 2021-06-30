fe26cb08d40f0bacbc996ea92f08c49d44f8165a
snowlotus <541364125@qq.com>
2021-06-30 16:03:48 +0800

    Update 334. Increasing Triplet Subsequence.cpp

--- math/334. Increasing Triplet Subsequence.cpp
+++ math/334. Increasing Triplet Subsequence.cpp
@@ -3,11 +3,13 @@ public:
     bool increasingTriplet(vector<int>& nums) {
         int x = INT_MAX, y = INT_MAX;
         // use 3 nums to record
+        // NOTICE: x is ONLY the smallest among TRIPLET
+        //         x IS NOT the smallest among ALL numbers
         for (auto num : nums) {
             if (num <= x)
-                x = num; // makes x ALWAYS the SMALLEST
+                x = num; // makes x ALWAYS the SMALLEST in these 3 nums
             else if (num <= y)
-                y = num; // makes y to be the SECOND SMALLEST
+                y = num; // makes y to be the SECOND SMALLEST in these 3 nums
             else
                 return true; // in this case, we found x < y < num
         }
