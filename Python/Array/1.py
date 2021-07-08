1b5d469c0eb814a52fb9b7f62c5a318e6d132e80
RollingWang <55792865+RollingWang@users.noreply.github.com>
2021-07-08 22:49:21 +0800

    Update 1.py

--- Python/Array/1.py
+++ Python/Array/1.py
@@ -1,1 +1,16 @@
-
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dct = {}
+        for i,n in enumerate(nums):
+            ob=target-n
+            if ob in dct:
+                #print(obj)
+                return [dct[ob],i]
+            else:
+                dct[n] = i
+        
