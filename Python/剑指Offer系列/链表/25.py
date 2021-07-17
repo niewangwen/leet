4c694f7187efe3daf51c9672f6f41a8ad6f737ee
RollingWang <55792865+RollingWang@users.noreply.github.com>
2021-07-17 21:35:15 +0800

    Create 25.py

---
+++ Python/剑指Offer系列/链表/25.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=21 lang=python3
+#
+# [21] 合并两个有序链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        cur = dum = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next, l1 = l1, l1.next
+            else:
+                cur.next, l2 = l2, l2.next
+            cur = cur.next
+        cur.next = l1 if l1 else l2
+        return dum.next
+        #时间复杂度：O(m + n)O(m+n)，m和n分别为两链表长度
+        #空间复杂度：O(1)O(1)
+
+# @lc code=end
+
