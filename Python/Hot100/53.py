aac4d93edf8921c24199b66a98bda71c9b686ef8
RollingWang <55792865+RollingWang@users.noreply.github.com>
2021-07-27 22:53:34 +0800

    Delete 53.py

--- Python/Hot100/53.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        res = nums[0]
-        dp = [0]*len(nums)
-        dp[0] = nums[0]
-        for  i in range(1,len(nums)):
-            dp[i] = max(dp[i-1]+nums[i],nums[i])
-            res  = max(res,dp[i])  
-        return res
